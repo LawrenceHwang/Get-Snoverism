39f5037ebcff96166feb23c7ecb82486a3647c4d
Lawrence Hwang <CoffeePoweredLion@gmail.com~>
2021-02-09 14:49:31 -0800

    Apply VSCode autoformatter.
    Format code

--- Get-Snoverism.ps1
+++ Get-Snoverism.ps1
@@ -1,5 +1,5 @@
 function Get-Snoverism {
-<#
+    <#
 .Synopsis
    Getting the quotes from the website: http://snoverisms.com/.
 .DESCRIPTION
@@ -17,22 +17,21 @@ function Get-Snoverism {
         # When used, the computer will output the text in speech as well.
         [switch]$talk
     )
-    Begin{
-        $result = (Invoke-WebRequest -Uri $uri).Images.title | Where-Object {$_} | Get-Random
+    Begin {
+        $result = (Invoke-WebRequest -Uri $uri).Images.title | Where-Object { $_ } | Get-Random
         Write-Verbose "Result is $result"
     }
-    Process{
+    Process {
 
         return $result
 
-        if ($talk){
+        if ($talk) {
             Write-Verbose "-talk parameter specified."
             Add-Type -AssemblyName System.speech
             $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
             $speak.Speak("$result")
         }
     }
-    End{
+    End {
     }
 }
-
